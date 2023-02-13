cea4c486fb4709e26a43c64b76f0c4a8e081c828
splace271 <94423485+splace271@users.noreply.github.com>
2023-02-13 18:54:24 +0700

    week1
    login/home/detail

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { ResetPasswordComponent } from './auth/components/reset-password/reset-password.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    ResetPasswordComponent,
+    HomeComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
