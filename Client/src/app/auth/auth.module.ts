f05e7dfda9eaf0b717663750438b73e9677eaae6
splace271 <94423485+splace271@users.noreply.github.com>
2023-02-17 19:41:16 +0700

    home

--- Client/src/app/auth/auth.module.ts
+++ Client/src/app/auth/auth.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IdeaComponentComponent } from './components/idea-component/idea-component.component';
-import { IdeaComponent } from './components/idea/idea.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 
 
 @NgModule({
   declarations: [
-  
-    IdeaComponentComponent,
-       IdeaComponent,
-       SidebarComponent
+    
   ],
   imports: [
     CommonModule
