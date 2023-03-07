7482040056805283eea86d098a6b212a9092ff8d
NerDucc <duc.ngovan225@gmail.com>
2023-03-07 11:58:58 +0700

    Second Update-Me-API

--- be/src/modules/me/me.module.ts
+++ be/src/modules/me/me.module.ts
@@ -3,11 +3,13 @@ import { MeService } from './me.service';
 import { MeController } from './me.controller';
 import { User } from '@core/database/mysql/entity/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserDetailService } from '@modules/user-detail/user-detail.service';
+import { UserDetailModule } from '@modules/user-detail/user-detail.module';
 
 @Module({
   controllers: [MeController],
   providers: [MeService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), UserDetailModule],
   exports: [MeService],
 })
 export class MeModule {}
