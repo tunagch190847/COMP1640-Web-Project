8b9aadcf174632b6478b168b101274a5fe8e7b94
Vuong Vu <vuongvu061101@gmail.com>
2023-03-10 09:32:49 +0700

    added some columns to the table comment

--- be/src/core/database/mysql/entity/comment.entity.ts
+++ be/src/core/database/mysql/entity/comment.entity.ts
@@ -1,3 +1,4 @@
+import { EIsDelete } from 'enum';
 import {
   Column,
   CreateDateColumn,
@@ -18,11 +19,26 @@ export class IdeaComment {
   @Column({ name: 'idea_id', type: 'int', unsigned: true })
   idea_id: number;
 
-  @Column('uuid', { name: 'user_id' })
-  user_id: string;
+  @Column('uuid', { name: 'author_id' })
+  author_id: string;
+
+  @Column('uuid', { name: 'reply_for_id', nullable: true })
+  reply_for_id: string;
+
+  @Column({ name: 'level', type: 'tinyint', unsigned: true, default: 1 })
+  level: number;
 
   @Column({ name: 'content', type: 'varchar', length: 800 })
   content: string;
+
+  @Column({
+    name: 'is_deleted',
+    type: 'tinyint',
+    width: 1,
+    comment: '0: not deleted, 1: deleted',
+    default: EIsDelete.NOT_DELETE,
+  })
+  is_deleted: number;
 
   @CreateDateColumn({ name: 'created_at' })
   created_at: Date;
@@ -30,11 +46,15 @@ export class IdeaComment {
   @UpdateDateColumn({ name: 'updated_at' })
   updated_at: Date;
 
-  @ManyToOne(() => Idea, { onUpdate: 'CASCADE' })
+  @ManyToOne(() => Idea, (idea) => idea.comments, { onUpdate: 'CASCADE' })
   @JoinColumn({ name: 'idea_id', referencedColumnName: 'idea_id' })
   idea: Idea;
 
-  @ManyToOne(() => User, (user) => user.ideas, { onUpdate: 'CASCADE' })
-  @JoinColumn({ name: 'user_id', referencedColumnName: 'user_id' })
-  user: User;
+  @ManyToOne(() => User, { onUpdate: 'CASCADE' })
+  @JoinColumn({ name: 'author_id', referencedColumnName: 'user_id' })
+  author: User;
+
+  @ManyToOne(() => User, { onUpdate: 'CASCADE' })
+  @JoinColumn({ name: 'reply_for_id', referencedColumnName: 'user_id' })
+  reply_for: User;
 }
