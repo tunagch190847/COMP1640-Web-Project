47c7b4c43f627d5c046ee4708dba966a779a3766
splace271 <94423485+splace271@users.noreply.github.com>
2023-03-11 10:20:20 +0700

    category

--- Client/src/app/manage-category/manage-category.component.ts
+++ Client/src/app/manage-category/manage-category.component.ts
@@ -1,21 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
-import { ProductServiceService } from '../services/product-service.service';
+import { Router } from '@angular/router';
 import { ConfirmationService } from 'primeng/api';
 import { MessageService } from 'primeng/api';
+import { AuthenticationService } from '../auth/services/authentication.service';
 
-interface Product {
-  id?: string;
-  code?: string;
-  name?: string;
-  description?: string;
-  price?: number;
-  quantity?: number;
-  inventoryStatus?: string;
-  category?: string;
-  image?: string;
-  rating?: number;
-}
+
 
 @Component({
   selector: 'app-manage-category',
@@ -26,111 +16,96 @@ interface Product {
 
 
 export class ManageCategoryComponent implements OnInit {
-  productDialog: boolean;
-
-  products: Product[];
-
-  product: Product;
-
-  selectedProducts: Product[];
-
-  submitted: boolean;
-
-  statuses: any[];
-
-  constructor(private productService: ProductServiceService, private messageService: MessageService, private confirmationService: ConfirmationService) { }
+  breadcrumb: any[];
+  cols: Array<any> = [];
+  listData: any[] = [];
+  displayXoa: boolean;
+  displayXoaN: boolean;
+  id: number;
+  name: string;
+  apiUrl: string = "http://localhost:3009/api/category";
+  listSelectedData: Array<any> = [];
+  constructor(private messageService: MessageService, private confirmationService: ConfirmationService, private router : Router,
+    private http : HttpClient, private authService: AuthenticationService) { 
+    this.getAllData();
+  }
 
   ngOnInit() {
-    this.productService.getProducts().then(data => this.products = data);
+    this.breadcrumb = [
+      { label: 'Tổng quan', url: '/home' },
+      { label: 'Tổ', url: '' },
+      { label: 'Danh sách category', url: '' },
+    ];
 
-    this.statuses = [
-      { label: 'INSTOCK', value: 'instock' },
-      { label: 'LOWSTOCK', value: 'lowstock' },
-      { label: 'OUTOFSTOCK', value: 'outofstock' }
+    this.cols = [
+      { field: 'Stt', header: 'STT', width: '50px', textAlign: 'center' },
+      { field: 'name', header: 'Name', width: '300px', textAlign: 'center' },
+      {
+        field: 'ThaoTac',
+        header: 'Thao tác',
+        width: '120px',
+        textAlign: 'center',
+      },
     ];
   }
-
-  openNew() {
-    this.product = {};
-    this.submitted = false;
-    this.productDialog = true;
+  
+  async getAllData() {
+    this.http.get<any>(this.apiUrl, {headers: {
+      Authorization: 'Bearer ' + this.authService.getToken()}
+    }).subscribe((result: any) => {
+            if (result.status_code != 200) {
+              this.showMessage('error', result.error_message);
+              return;
+            }
+            console.log(result)
+            this.listData = result.data.map((item, index) => Object.assign({
+              Stt: index + 1,
+            }, item));
+            console.log(this.listData)
+        });
+    
+    
   }
 
-  deleteSelectedProducts() {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete the selected products?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.products = this.products.filter(val => !this.selectedProducts.includes(val));
-        this.selectedProducts = null;
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
+  chinhSua(data) {
+    this.router.navigateByUrl('/');
+  }
+
+  showDialogXoa(data) {
+    this.displayXoa = true;
+    this.id = data.category_id;
+  }
+
+  showDialogXoaN() {
+    this.displayXoaN = true;
+  }
+
+  xoaNTo() {
+    if(this.listSelectedData.length){
+      for(let i = 0; i < this.listSelectedData.length; i++) {
+        this.id = this.listSelectedData[i].category_id;
+        this.xoaTo();
       }
-    });
-  }
-
-  editProduct(product: Product) {
-    this.product = { ...product };
-    this.productDialog = true;
-  }
-
-  deleteProduct(product: Product) {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete ' + product.name + '?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.products = this.products.filter(val => val.id !== product.id);
-        this.product = {};
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
-      }
-    });
-  }
-
-  hideDialog() {
-    this.productDialog = false;
-    this.submitted = false;
-  }
-
-  saveProduct() {
-    this.submitted = true;
-
-    if (this.product.name.trim()) {
-      if (this.product.id) {
-        this.products[this.findIndexById(this.product.id)] = this.product;
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
-      }
-      else {
-        this.product.id = this.createId();
-        this.product.image = 'product-placeholder.svg';
-        this.products.push(this.product);
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
-      }
-
-      this.products = [...this.products];
-      this.productDialog = false;
-      this.product = {};
+      this.listSelectedData = null;
+    }else{
+      this.displayXoaN = false;
     }
+    
   }
 
-  findIndexById(id: string): number {
-    let index = -1;
-    for (let i = 0; i < this.products.length; i++) {
-      if (this.products[i].id === id) {
-        index = i;
-        break;
-      }
-    }
-
-    return index;
+  async xoaTo() { 
+    this.http.delete(this.apiUrl +'/'+ this.id, {headers: {
+      Authorization: 'Bearer ' + this.authService.getToken()}
+    }).subscribe(() => 
+        this.showMessage('success', 'Delete success'));
+    this.displayXoa = false;
+    this.displayXoaN = false;
+    
+    this.getAllData();
   }
 
-  createId(): string {
-    let id = '';
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (var i = 0; i < 5; i++) {
-      id += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return id;
+  showMessage(severity: string, detail: string) {
+    this.messageService.add({ severity: severity, summary: 'Thông báo:', detail: detail });
   }
+
 }
